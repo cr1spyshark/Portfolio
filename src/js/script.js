b18e7422c0dac3b1dc9d2ba2a3ed169a418d02f2
Kirill <example@gmail.com>
2022-06-26 15:56:38 +0300

    fifi

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      overlay = document.querySelector('.menu__overlay'),
+      close = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', function() {
+    menu.classList.add('active');
+});
+
+close.addEventListener('click', function() {
+    menu.classList.remove('active');
+}); 
+
+overlay.addEventListener('click', function() {
+    menu.classList.remove('active');
+});
